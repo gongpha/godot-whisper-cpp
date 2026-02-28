16b5702e204bad6bec7461e0034d8bde2e1f1172
Kongfa Waroros <gongpha@hotmail.com>
2026-03-01 04:16:37 +0700

    add docs (best-effort ^^;) & LICENSE

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 def can_build(env, platform):
     return True
 
+
 def configure(env):
     pass
+
+
+def get_doc_classes():
+    return [
+        "ResourceFormatLoaderWhisperModel"
+        "Whisper",
+        "WhisperAudio",
+        "WhisperModel",
+        "WhisperResult",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
---
+++ project/README.md
@@ -0,0 +1,3 @@
+# Example Project
+
+This example project picked an audio sample and an audio converter from [V-Sekai's godot-whisper](https://github.com/V-Sekai/godot-whisper).
